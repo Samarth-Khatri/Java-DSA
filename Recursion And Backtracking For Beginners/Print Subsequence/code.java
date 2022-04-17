a95658291ae4067f96637f9db39db8ee7a52b9f3
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-04-17 21:32:36 +0530

    Added ques

---
+++ Recursion And Backtracking For Beginners/Print Subsequence/code.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        printSS(str,"");
+    }
+
+    public static void printSS(String str, String ans) {
+        if(str.length()==0) {
+            System.out.println(ans);
+            return;
+        }
+        
+        char ch = str.charAt(0);
+        str = str.substring(1);
+        printSS(str,ans+ch); // accept
+        printSS(str,ans); // reject
+    }
+
+}
