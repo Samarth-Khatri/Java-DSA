b2894278d1844e4c96012e31d1f89e285eeec679
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-06-25 02:59:59 +0530

    Added Ques test

---
+++ Recursion And Backtracking For Beginners/Knights Tour/code.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        
+    }
+
+    public static void printKnightsTour(int[][] chess, int r, int c, int upcomingMove) {
+        
+    }
+
+    public static void displayBoard(int[][] chess){
+        for(int i = 0; i < chess.length; i++){
+            for(int j = 0; j < chess[0].length; j++){
+                System.out.print(chess[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+    }
+}
