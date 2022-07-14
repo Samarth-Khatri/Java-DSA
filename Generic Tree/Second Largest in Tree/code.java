c6f1a295a2a306d10e6b50f7a34c76b04935c47a
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-07-15 03:00:43 +0530

    Added ques

---
+++ Generic Tree/Second Largest in Tree/code.java
@@ -0,0 +1,77 @@
+import java.io.*;
+ 
+import java.util.*;
+ 
+public class Main {
+  private static class Node {
+    int data;
+    ArrayList< Node> children = new ArrayList< >();
+  }
+ 
+  public static void display(Node node) {
+    String str = node.data + " -> ";
+    for (Node child : node.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+ 
+    for (Node child : node.children) {
+      display(child);
+    }
+  }
+ 
+  public static Node construct(int[] arr) {
+    Node root = null;
+ 
+    Stack< Node> st = new Stack< >();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+ 
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+ 
+        st.push(t);
+      }
+    }
+ 
+    return root;
+  }
+
+  static int largest = Integer.MIN_VALUE;
+  static int slargest = Integer.MIN_VALUE;
+
+  public static void secondLargest(Node node) {
+    if(node.data>largest) {
+        slargest = largest;
+        largest = node.data;
+    } 
+    else if(node.data>slargest)
+        slargest = node.data;
+    for(Node child : node.children)
+        secondLargest(child);
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(values[i]);
+    }
+ 
+    Node root = construct(arr);
+    secondLargest(root);
+    System.out.println(largest);
+    System.out.println(slargest);
+  }
+ 
+}
