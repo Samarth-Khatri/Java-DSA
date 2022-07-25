6a000659f0882e12b07100b02cf72d41e341ff7b
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-07-25 18:44:53 +0530

    Updated

--- HashMap and Heap/Highest Frequency Character/code.java
+++ HashMap and Heap/Highest Frequency Character/code.java
@@ -8,7 +8,7 @@ public class Main {
         String str = scn.next();
         HashMap<Character, Integer> map = new HashMap<>();
         for(int i=0;i<str.length();++i) {
-            char ch = str.charAt(i);
+            Character ch = str.charAt(i);
             if(map.containsKey(ch)) 
                 map.put(ch, map.get(ch)+1);
             else
