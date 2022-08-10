aa66912146aa099a703234dc35858bb13599f008
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-08-10 15:17:26 +0530

    Added ques

---
+++ HashMap and Heap/Sort K-sorted Array/code.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int n = Integer.parseInt(br.readLine());
+      int[] arr = new int[n];
+
+      for (int i = 0; i < n; i++) {
+         arr[i] = Integer.parseInt(br.readLine());
+      }
+
+      int k = Integer.parseInt(br.readLine());
+      
+      // Simple Approach = Tc -> O(NlogN), Sc -> O(N)
+      PriorityQueue<Integer> pq = new PriorityQueue<>();
+      for(int i=0;i<n;++i)
+         pq.add(arr[i]);
+      for(int i=0;i<n;++i)
+         System.out.println(pq.remove());
+
+      // Better Approach = Tc -> O(Nlogk), Sc -> O(k)
+      PriorityQueue<Integer> pq = new PriorityQueue<>();
+      int i=0;
+      for(;i<k+1;++i)
+         pq.add(arr[i]);
+      for(;i<n;++i) {
+            System.out.println(pq.remove());
+            pq.add(arr[i]);
+      }
+      while(pq.size() > 0)
+         System.out.println(pq.remove());
+   }
+
+}
