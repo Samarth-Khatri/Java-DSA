216c8befc6a4aef57cd4a4e81814ac8845b26d3f
abc <abc@example.com>
2022-08-25 19:23:14 +0530

    Added Ques

---
+++ Graphs/Has Path/code.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+   static class Edge {
+      int src;
+      int nbr;
+      int wt;
+
+      Edge(int src, int nbr, int wt){
+         this.src = src;
+         this.nbr = nbr;
+         this.wt = wt;
+      }
+   }
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int vtces = Integer.parseInt(br.readLine());
+      ArrayList<Edge>[] graph = new ArrayList[vtces];
+      for(int i = 0; i < vtces; i++){
+         graph[i] = new ArrayList<>();
+      }
+
+      int edges = Integer.parseInt(br.readLine());
+      for(int i = 0; i < edges; i++){
+         String[] parts = br.readLine().split(" ");
+         int v1 = Integer.parseInt(parts[0]);
+         int v2 = Integer.parseInt(parts[1]);
+         int wt = Integer.parseInt(parts[2]);
+         graph[v1].add(new Edge(v1, v2, wt));
+         graph[v2].add(new Edge(v2, v1, wt));
+      }
+
+      int src = Integer.parseInt(br.readLine());
+      int dest = Integer.parseInt(br.readLine());
+
+      boolean []visited = new boolean[vtces]; // boolean array equal to no. of vertices
+      boolean pathExists = hasPath(graph, visited, src, dest);
+      System.out.println(pathExists);
+    }
+
+    public static boolean hasPath(ArrayList<Edge> []graph, boolean []visited, int src, int dest) {
+       if(src == dest) 
+         return true;
+
+      visited[src] = true;
+      for(int i=0;i<graph[src].size();++i) {
+         Edge edge = graph[src].get(i);
+         int nbr = edge.nbr; // neighbour vertex
+         if(!visited[nbr])
+            if(hasPath(graph, visited, nbr, dest))
+               return true;
+      }
+      return false;
+    }
+
+}
