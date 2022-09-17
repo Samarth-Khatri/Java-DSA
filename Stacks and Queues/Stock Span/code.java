32d5b6a1af078893cdfe97fdf1b01ce389c4630e
abc <abc@example.com>
2022-09-17 15:48:09 +0530

    Added Ques

--- Stacks and Queues/Stock Span/code.java
+++ Stacks and Queues/Stock Span/code.java
@@ -20,12 +20,12 @@ public static void main(String[] args) throws Exception {
        a[i] = Integer.parseInt(br.readLine());
     }
 
-    int[] span = solve(a);
+    int[] span = solve2(a);
     display(span);
  }
 
 // Can also be done by making a class pair
-// Pushing the index in stack instead of arr[i] 
+// Pushing the index in stack instead of arr[i] from left
  public static int[] solve(int[] arr) {
     int[] ans = new int[arr.length];
     Stack<Integer> st = new Stack<>();
@@ -37,6 +37,21 @@ public static void main(String[] args) throws Exception {
       else ans[i] = i - st.peek();
       st.push(i);
     }
+    return ans;
+ }
+ 
+ // Second Approach from right
+ public static int[] solve2(int[] arr) {
+    int[] ans = new int[arr.length];
+    Stack<Integer> st = new Stack<>();
+    st.push(arr.length-1);
+    for(int i=arr.length-2;i>=0;--i) {
+      while(!st.isEmpty() && arr[i] >= arr[st.peek()]) 
+        ans[st.peek()] = st.pop() - i;
+      st.push(i);
+    }
+    while(!st.isEmpty()) 
+      ans[st.peek()] = st.pop() + 1; 
     return ans;
  }
 
