386202204fde6359964f4d4f5e31d1c1bb2a8ada
abc <abc@example.com>
2022-10-18 19:57:35 +0530

    Updated

--- HashMap and Heap/Write Hashmap/code.java
+++ HashMap and Heap/Write Hashmap/code.java
@@ -3,96 +3,90 @@ import java.util.*;
 
 public class Main {
 
-  public static class HashMap<K,V> {
-    private class HMPair {
+  public static class HashMap<K, V> {
+    private class HMNode {
       K key;
-      V val;
+      V value;
 
-      HMPair(K key, V val) {
+      HMNode(K key, V value) {
         this.key = key;
-        this.val = val;
+        this.value = value;
       }
     }
 
-    int size; // n
-    LinkedList<HMPair>[] buckets; // N = buckets.length
+    private int size; // n
+    private LinkedList<HMNode>[] buckets; // N = buckets.length
 
     public HashMap() {
       initbuckets(4);
       size = 0;
     }
 
-    private void initbuckets(int N) {
-      buckets = new LinkedList[N];
-      for (int bi = 0; bi < buckets.length; ++bi) {
-        buckets[bi] = new LinkedList<>();
+    public void put(K key, V value) throws Exception {
+      int bi = hashFunction(key);
+      int di = findInBucket(bi, key);
+
+      if (di == -1) {
+        HMNode node = new HMNode(key, value);
+        buckets[bi].addLast(node);
+        size++;
+      } else {
+        HMNode node = buckets[bi].get(di);
+        node.value = value;
+      }
+
+      double lambda = size * 1.0 / buckets.length;
+      if (lambda > 2.0) {
+        rehash();
+      }
+    }
+
+    public V get(K key) throws Exception {
+      int bi = hashFunction(key);
+      int di = findInBucket(bi, key);
+
+      if (di == -1) {
+        return null;
+      } else {
+        HMNode node = buckets[bi].get(di);
+        return node.value;
       }
     }
 
     public boolean containsKey(K key) {
       int bi = hashFunction(key);
-      int di = findInBucket(key,bi);
-      return (di != -1);
-    }
+      int di = findInBucket(bi, key);
 
-    public V get(K key) {
-      int bi = hashFunction(key);
-      int di = findInBucket(key,bi);
-      if(di == -1) return null;
-      else {
-        HMPair pair = buckets[bi].get(di);
-        return pair.val;
+      if (di == -1) {
+        return false;
+      } else {
+        return true;
       }
     }
 
-    public V remove(K key) {
+    public V remove(K key) throws Exception {
       int bi = hashFunction(key);
-      int di = findInBucket(key,bi);
-      if(di == -1) return null;
-      else {
-        HMPair pair = buckets[bi].remove(di);
+      int di = findInBucket(bi, key);
+
+      if (di == -1) {
+        return null;
+      } else {
+        HMNode node = buckets[bi].remove(di);
         size--;
-        return pair.val;
+        return node.value;
       }
     }
 
-    public void put(K key, V val) {
-      int bi = hashFunction(key);
-      int di = findInBucket(key,bi);
-      if(di == -1) {
-        HMPair pair = new HMPair(key,val);
-        buckets[bi].add(pair);
-        size++;
-      }
-      else {
-        HMPair pair = buckets[bi].get(di);
-        pair.val = val;
-      }
+    public ArrayList<K> keyset() throws Exception {
+      ArrayList<K> set = new ArrayList<>();
 
-      double lambda = size * 1.0 / buckets.length;
-      if(lambda > 2.0) rehash(); 
-    }
-
-    private void rehash() {
-      LinkedList<HMPair>[] oba = buckets;
-      size = 0;
-      initbuckets(2*oba.length);
-      for(int bi = 0; bi < oba.length; ++bi) {
-        for(int di = 0; di < buckets[bi].size(); ++di) {
-          HMPair pair = oba[bi].get(di);
-          put(pair.key, pair.val);
+      for (int bi = 0; bi < buckets.length; bi++) {
+        for (HMNode node : buckets[bi]) {
+          set.add(node.key);
         }
       }
-    }
 
-    public ArrayList<K> keyset() {
-      ArrayList<K> keys = new ArrayList<K>();
-      for(int bi = 0; bi < buckets.length; ++bi) 
-        for(int di = 0; di < buckets[bi].size(); ++di) {
-          HMPair pair = buckets[bi].get(di);
-          keys.add(pair.key);
-        }
-        return keys;
+      return set;
     }
 
     public int size() {
@@ -103,25 +97,52 @@ public class Main {
       System.out.println("Display Begins");
       for (int bi = 0; bi < buckets.length; bi++) {
         System.out.print("Bucket" + bi + " ");
-        for (HMPair pair : buckets[bi]) 
-          System.out.print(pair.key + "@" + pair.val + " ");
+        for (HMNode node : buckets[bi]) {
+          System.out.print( node.key + "@" + node.value + " ");
+        }
         System.out.println(".");
       }
       System.out.println("Display Ends");
     }
-
+    
+    // returns bucket index for a key
     private int hashFunction(K key) {
-      int hash = key.hashCode();
-      int bi = Math.abs(hash) % buckets.length; // compression
+      int hc = key.hashCode();
+      int bi = Math.abs(hc) % buckets.length;
       return bi;
     }
 
-    private int findInBucket(K key, int bi) {
-      for(int di = 0; di < buckets[bi].size(); ++di) {
-        HMPair pair = buckets[bi].get(di);
-        if(pair.key.equals(key)) return di;
+    // return data index for a bucket and key
+    private int findInBucket(int bi, K key) {
+      int di = 0;
+      for (HMNode node : buckets[bi]) {
+        if (node.key.equals(key)) {
+          return di;
+        }
+        di++;
       }
+
       return -1;
+    }
+
+    // when lambda crosses a threshold
+    private void rehash() throws Exception {
+      LinkedList<HMNode>[] oba = buckets;
+      initbuckets(2 * oba.length);
+      size = 0;
+
+      for (int bi = 0; bi < oba.length; bi++) {
+        for (HMNode onode : oba[bi]) {
+          put(onode.key, onode.value);
+        }
+      }
+    }
+
+    private void initbuckets(int N) {
+      buckets = new LinkedList[N];
+      for (int bi = 0; bi < buckets.length; bi++) {
+        buckets[bi] = new LinkedList<>();
+      }
     }
   }
 
