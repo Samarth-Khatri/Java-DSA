295991727cb5a346f189c0b9f80487390bb20df5
abc <abc@example.com>
2022-11-17 03:25:22 +0530

    Updated

--- Level 2/Recursion_Revision/code.java
+++ Level 2/Recursion_Revision/code.java
@@ -87,5 +87,32 @@ public class Main {
 
     return count;
   }
-  
+
+  // jump : 1,2,3
+  public static int stairPath(int n, String ans) {
+    if (n == 0) {
+      System.out.println(ans);
+      return 1;
+    }
+
+    int count = 0;
+    for (int jump = 1; jump <= 3 && n - jump >= 0; jump++) 
+      count += stairPath(n - jump, ans + jump);
+
+    return count;
+  }
+
+  // jumps : [1,4,6,7]
+  public static int variablePath(int[] jumps, int n, String ans) {
+    if (n == 0) {
+      System.out.println(ans);
+      return 1;
+    }
+
+    int count = 0;
+    for (int i = 0; i < jumps.length && n - jumps[i] >= 0; i++) 
+      count += variablePath(jumps, n - jumps[i], ans + jumps[i]);
+
+    return count;
+  }
 }
