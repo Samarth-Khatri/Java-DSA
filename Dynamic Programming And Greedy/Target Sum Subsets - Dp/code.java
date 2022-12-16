5b8db10417cd716c56770d79f364d9abd7898e1c
abc <abc@example.com>
2022-12-16 22:05:32 +0530

    Added Ques

--- Dynamic Programming And Greedy/Target Sum Subsets - Dp/code.java
+++ Dynamic Programming And Greedy/Target Sum Subsets - Dp/code.java
@@ -9,9 +9,27 @@ public class Main {
     int[] arr = new int[n];
     for (int i = 0; i < n; ++i) arr[i] = scn.nextInt();
     int tar = scn.nextInt();
-    System.out.println(
-      sumSub_mem(arr, 0, tar, new Boolean[arr.length + 1][tar + 1])
-    );
+    
+    // System.out.println(
+    //   sumSub_mem(arr, 0, tar, new Boolean[arr.length + 1][tar + 1])
+    // );
+
+    Boolean[][] strg = new Boolean[n + 1][tar + 1];
+
+    for (int i = 0; i < strg.length; ++i) {
+      for (int j = 0; j < strg[0].length; ++j) {
+        if (i == 0 && j == 0) strg[i][j] = true; 
+        else if (i == 0) strg[i][j] = false; 
+        else if (j == 0) strg[i][j] = true; 
+        else {
+          strg[i][j] = strg[i - 1][j];
+          int val = arr[i - 1];
+          if (j >= val && strg[i - 1][j - val]) strg[i][j] = true;
+        }
+      }
+    }
+
+    System.out.println(strg[n][tar]);
   }
 
   public static boolean sumSub_mem(int[] arr, int idx, int tar, Boolean[][] strg) {
