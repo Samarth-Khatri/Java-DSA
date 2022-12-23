82a94ef9a4e1f39d3e36a23f151ba3d7e98443da
abc <abc@example.com>
2022-12-23 23:53:44 +0530

    Updated

--- Dynamic Programming And Greedy/Arrange Buildings/code.java
+++ Dynamic Programming And Greedy/Arrange Buildings/code.java
@@ -6,14 +6,14 @@ public class Main {
 public static void main(String[] args) throws Exception {
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
-    int[][] dp = new int[2][n + 1];
+    long[][] dp = new long[2][n + 1];
     dp[0][1] = 1;
     dp[1][1] = 1;
     for (int i = 2; i <= n; ++i) {
       dp[0][i] = dp[1][i - 1];
       dp[1][i] = dp[0][i - 1] + dp[1][i - 1];
     }
-    int ans = dp[0][n] + dp[1][n]; // config for one side of road
+    long ans = dp[0][n] + dp[1][n]; // config for one side of road
     System.out.println(ans*ans); // for buildings on both sides of roads
  }
 
